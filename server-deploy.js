e05d957b836f24bc45ef4cc3f50eb00a853be315
Jakekato <loveillusion02@gmail.com>
2023-09-05 18:34:08 +0700

    dev & deploy mode division

---
+++ server-deploy.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const publicDir = path.join(__dirname, 'public');
+const port = 3000;
+
+const app = new express();
+app.set('port', process.env.PORT || port)
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(publicDir, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server app listening on port ${port}`)
+})
